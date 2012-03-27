d2eba88928927247b77bb993b604470bc74d78df
Peter Smogner <psmogner@gmail.com>
2012-03-27 16:27:30 +0200

    fixed getters + setters

--- src/HttpRequest.java
+++ src/HttpRequest.java
@@ -12,22 +12,70 @@ public class HttpRequest {
 	public HttpRequest(){
 		
 	}
-	
-	public String getMethod_name() {return method_name;}
-	public String getLocal_path() {return local_path;}
-	public String getProtocol_version() {return protocol_version;}
-	public String getFrom(){return mailFrom;}
-	public String getTo(){return mailTo;}
-	public String getSubject(){return mailSubject;}
-	public String getSMTP(){return mailSMTP;}
-	public String getText(){return mailText;}
 
-	public String setFrom(String from){return mailFrom;}
-	public String setTo(String to){return mailTo;}
-	public String setSubject(String subject){return mailSubject;}
-	public String setSMTP(String SMTP){return mailSMTP;}
-	public String setText(String message){return mailText;}
-	public void setMethod_name(String method_name) {this.method_name = method_name;}
-	public void setLocal_path(String local_path) {this.local_path = local_path;}
-	public void setProtocol_version(String protocol_version) {this.protocol_version = protocol_version;}
+	public String getMethod_name() {
+		return method_name;
+	}
+
+	public void setMethod_name(String method_name) {
+		this.method_name = method_name;
+	}
+
+	public String getLocal_path() {
+		return local_path;
+	}
+
+	public void setLocal_path(String local_path) {
+		this.local_path = local_path;
+	}
+
+	public String getProtocol_version() {
+		return protocol_version;
+	}
+
+	public void setProtocol_version(String protocol_version) {
+		this.protocol_version = protocol_version;
+	}
+
+	public String getMailFrom() {
+		return mailFrom;
+	}
+
+	public void setMailFrom(String mailFrom) {
+		this.mailFrom = mailFrom;
+	}
+
+	public String getMailTo() {
+		return mailTo;
+	}
+
+	public void setMailTo(String mailTo) {
+		this.mailTo = mailTo;
+	}
+
+	public String getMailSubject() {
+		return mailSubject;
+	}
+
+	public void setMailSubject(String mailSubject) {
+		this.mailSubject = mailSubject;
+	}
+
+	public String getMailSMTP() {
+		return mailSMTP;
+	}
+
+	public void setMailSMTP(String mailSMTP) {
+		this.mailSMTP = mailSMTP;
+	}
+
+	public String getMailText() {
+		return mailText;
+	}
+
+	public void setMailText(String mailText) {
+		this.mailText = mailText;
+	}
+	
+
 }
